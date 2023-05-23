634beda5fb9f8308998a3416cc51458f4cc31ac1
jeromerobichez <jerome.robichez@gmail.com>
2023-05-23 23:44:48 +0200

    double modal OK center to finish

--- src/actorDetails.js
+++ src/actorDetails.js
@@ -4,18 +4,61 @@ import axios from 'axios';
 
 export const ActorDetails = ({ actorDetails}) => {
 
+  const [modalDisplayed, setModalDisplayed] = useState(false);
+  const [movieTitle, setMovieTitle] = useState("");
+  const [moviePitch, setMoviePitch] = useState("");
+  const [moviePoster, setMoviePoster] = useState("");
+  const [activeOverlay, setActiveOverlay] = useState(false);
+  const [message, setMessage] = useState(null);
+
+  const movieInfoDisplay = (id) => {
+
+    axios
+      .post('http://localhost:5000/movie-detail', { id })
+      /* .post("http://localhost:5000/movie-detail", {id}) */ //  http://localhost:5000/movie-detail
+      .then(res => {
+
+        setMovieTitle(res.data.title)
+        setMoviePitch(res.data.overview)
+        setMoviePoster(res.data.poster_path)
+        setModalDisplayed(true)
+        setActiveOverlay(true)
+
+      })
+      .catch(e => {
+        setMessage(`Erreur lors de la création : ${e.message}`)
+        console.log(message)
+      })
+  }
+  const closeModal = () => {
+    setActiveOverlay(false)
+    setModalDisplayed(false)
+
+  }
+
   if (actorDetails !== undefined) {
     const backgroundStyle = {
 
       backgroundImage: `url(https://image.tmdb.org/t/p/w500/${actorDetails.profile_path})`,
 
     }
-  
+
 return (
 <div className='modal-center'>
-    <div className="modal-container"
-   
-    >
+    <div className="modal-container">
+      <div className={activeOverlay ? 'second-overlay-active' : "second-overlay-inactive"}
+    onClick={closeModal}> </div>
+ <div className="movie-detail-modal"
+                style={{ display: modalDisplayed ? "flex" : "none" }}>
+                <h3>{movieTitle}</h3>
+                <img src={`https://image.tmdb.org/t/p/w500/${moviePoster}`}
+                  alt={movieTitle}
+                  className="modal-poster"
+                  width={150}
+                />
+                <h4>Summary :</h4>
+                <p>{moviePitch}</p>
+              </div>
 <div  className="modal-head"  style={backgroundStyle}>
         <h1 className="modal-title">
         {actorDetails.name}
@@ -33,11 +76,15 @@ return (
             <ul>
           {actorDetails.known_for !== undefined ? actorDetails.known_for.map((k, i)=>
         
-          <li style={{"list-style": "none"}} >
+          <li style={{"list-style": "none"}}
+           onClick={() => movieInfoDisplay(k.id)} >
             <span
             className='li-knownfor'
+            
             >{k.title}</span></li>): "coucou"}</ul>
          </p> 
+
+        
        {/*  <button className="modal__btn modal__btn--red">
           <PlayIcon className="modal__btn--icon" />
           Play
